bae69af99f95be98ad22e3606124e60c637e7310
jpokalsky <jakepokalsky@gmail.com>
2022-02-13 11:24:41 -0500

    added sticky nav bar desktop + mobile

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,106 @@
+
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+      v-model="drawer"
+      v-if="$vuetify.breakpoint.mobile"         
+      app>
+      <!-- <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Chimex
+          </v-list-item-title>
+          
+        </v-list-item-content>             
+
+      </v-list-item> -->
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+     <v-card class="overflow-hidden">
     <v-app-bar
       app
-      color="primary"
-      dark
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      
+      <v-app-bar-nav-icon
+        v-if="$vuetify.breakpoint.mobile"
+      >
+      </v-app-bar-nav-icon>      
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
+      <v-toolbar-title><v-img
+          alt="Chimex Logo"          
           contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          src="./assets/logo.png"
+          transition="scale-transition"
           width="100"
-        />
-      </div>
+        /></v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-7"
+      class="overflow-y-auto"
+      max-height="600"
+    >
 
-    <v-main>
-      <router-view/>
-    </v-main>
+      <v-main>
+        <!--  -->
+      </v-main>
+    
+    </v-sheet>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+     mounted () {
+      console.log(this.$vuetify.breakpoint)
+    },
+    data: () => ({ 
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'Residential', icon: 'mdi-image' },
+        { title: 'Commercial', icon: 'mdi-help-box' },
+        { title: 'About', icon: 'mdi-help-box' },
+      ], 
+    }),
+  }
 </script>
